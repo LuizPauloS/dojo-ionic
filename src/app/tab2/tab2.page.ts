fc7b14329cec69a7c8229d101c36c1f9bf59afd5
luizpaulosilva <luiz.paulo.09@hotmail.com>
2019-05-14 00:03:33 -0300

    Adicionando lista com avatar

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { SharedService } from './../shared/shared.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+
+  movies: Array<any>;
+
+  constructor(private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    this.movies = [];
+    this.getListMoviesApi();
+  }
+
+  getListMoviesApi(): void {
+    this.sharedService.getMoviesLatest().subscribe((response: any) => {
+      this.movies = response.results;
+    });
+  }
+
+}
